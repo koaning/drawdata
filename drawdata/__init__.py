059f92d3b540e8d6af769c7ea2ff34ee2efaa04c
koaning <vincentwarmerdam@gmail.com>
2021-04-11 20:49:26 +0200

    upgrade

--- drawdata/__init__.py
+++ drawdata/__init__.py
@@ -1,5 +1,13 @@
 from IPython.display import IFrame
 
 
+def draw_line():
+    return IFrame("https://drawdata.xyz/line.html", width=800, height=550)
+
+
 def draw_scatter():
-    return IFrame("https://drawdata.xyz/scatter.html", width=800, height=400)
+    return IFrame("https://drawdata.xyz/scatter.html", width=800, height=550)
+
+
+def draw_histogram():
+    return IFrame("https://drawdata.xyz/histogram.html", width=800, height=550)
