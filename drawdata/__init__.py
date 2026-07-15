726812387326c158f7e258c133b1dad57fc68bae
koaning <vincentwarmerdam@gmail.com>
2026-07-15 12:21:00 +0200

    Release 0.5.2
    Drop unused IPython.display import that broke imports under marimo's
    wasm runtime (molab), where IPython probes for a JupyterLite-specific
    `js.messenger` global at import time.

--- drawdata/__init__.py
+++ drawdata/__init__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
+
 import anywidget
 import traitlets
-from IPython.display import IFrame
 
 
 class ScatterWidget(anywidget.AnyWidget):
@@ -9,8 +9,9 @@ class ScatterWidget(anywidget.AnyWidget):
     A scatter drawing widget that automatically can update a pandas/polars dataframe
     as your draw data.
     """
-    _esm = Path(__file__).parent / 'static' / 'scatter_widget.js'
-    _css = Path(__file__).parent / 'static' / 'scatter_widget.css'
+
+    _esm = Path(__file__).parent / "static" / "scatter_widget.js"
+    _css = Path(__file__).parent / "static" / "scatter_widget.css"
     data = traitlets.List([]).tag(sync=True)
     brushsize = traitlets.Int(40).tag(sync=True)
     width = traitlets.Int(800).tag(sync=True)
@@ -20,31 +21,33 @@ class ScatterWidget(anywidget.AnyWidget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         if not 1 <= self.n_classes <= 4:
-            raise ValueError('n_classes must be between 1 and 4')
+            raise ValueError("n_classes must be between 1 and 4")
 
     @property
     def data_as_pandas(self):
-        import pandas as pd 
+        import pandas as pd
+
         return pd.DataFrame(self.data)
 
     @property
     def data_as_polars(self):
         import polars as pl
+
         return pl.DataFrame(self.data)
 
     @property
     def data_as_X_y(self):
         import numpy as np
 
-        colors = [_['color'] for _ in self.data]
-        
+        colors = [_["color"] for _ in self.data]
+
         # Assume that we're dealing with regression in this case
         if np.unique(colors).shape[0] == 1:
-            X = np.array([[_['x']] for _ in self.data])
-            y = np.array([_['y'] for _ in self.data])
+            X = np.array([[_["x"]] for _ in self.data])
+            y = np.array([_["y"] for _ in self.data])
             return X, y
-        
-        X = np.array([[_['x'], _['y']] for _ in self.data])
+
+        X = np.array([[_["x"], _["y"]] for _ in self.data])
         return X, colors
 
 
@@ -53,8 +56,9 @@ class BarWidget(anywidget.AnyWidget):
     A bar drawing widget that automatically can update a pandas/polars dataframe
     as your draw data.
     """
-    _esm = Path(__file__).parent / 'static' / 'bar_widget.js'
-    _css = Path(__file__).parent / 'static' / 'bar_widget.css'
+
+    _esm = Path(__file__).parent / "static" / "bar_widget.js"
+    _css = Path(__file__).parent / "static" / "bar_widget.css"
     data = traitlets.List([]).tag(sync=True)
     y_min = traitlets.Float(0.0).tag(sync=True)
     y_max = traitlets.Float(1.0).tag(sync=True)
@@ -62,12 +66,15 @@ class BarWidget(anywidget.AnyWidget):
     width = traitlets.Int(800).tag(sync=True)
     height = traitlets.Int(550).tag(sync=True)
     collection_names = traitlets.List(["collection1", "collection2"]).tag(sync=True)
+
     @property
     def data_as_pandas(self):
-        import pandas as pd 
+        import pandas as pd
+
         return pd.DataFrame(self.data)
 
     @property
     def data_as_polars(self):
         import polars as pl
+
         return pl.DataFrame(self.data)
